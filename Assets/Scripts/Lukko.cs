e4ab6ecf857ee370d484f06687a47dfe240aad08
TaneliR <marcu@dnainternet.net>
2017-01-17 14:01:56 +0200

    Simplified the lock script

---
+++ Assets/Scripts/Lukko.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+//-------------------------------------------------------------------------
+
+public class Lukko : MonoBehaviour
+{
+    public GameObject ovi; // Referenssi siltä varalta, ettei trigger ole animaation sisältävän objektin lapsi
+
+    //-------------------------------------------------
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "avain")
+        {
+            Debug.Log("AVAIN OSUI");
+            ovi.GetComponentInParent<Animator>().SetTrigger("Avain");
+        }
+    }
+}
